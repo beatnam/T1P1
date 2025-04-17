43fd09dfbd479c4e1c8dc9ba82be3d79f900975b
star5 <star5@Nakamura-Kim>
2025-04-17 20:49:34 +0900

    Filtering

---
+++ Team1Project1/src/main/java/com/itwillbs/controller/FilteringController.java
@@ -0,0 +1,15 @@
+package com.itwillbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FilteringController {
+
+	
+	@PostMapping("/filtering/filterPro")
+	public String FilterPro() {
+		
+		return null;
+	}
+}
