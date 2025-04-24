b55e1743e869721df7770aa30cb3c47646b34ee3
kmskim <mnai2000@naver.com>
2025-04-24 17:54:28 +0900

    2025 04 24 1754 로그인 완료

--- Team1Project1/src/main/java/com/itwillbs/controller/MemberController.java
+++ Team1Project1/src/main/java/com/itwillbs/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.itwillbs.controller;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,13 +41,36 @@ public class MemberController {
 	
 	@GetMapping("/login")
 	public String loginHome() {
+		System.out.println("memberController login");
+		
 		
 		return "member/login";
 	}
 	
+	@PostMapping("/loginPro")
+	public String loginPro(MemberDTO memberDTO, HttpSession session) {
+		System.out.println("memberController loginPro");
+		
+		
+		MemberDTO memberDTO2 = memberService.loginMember(memberDTO);
+		if(memberDTO2 != null) {
+			//아이디 비밀번호 일치
+			//로그인 성공 => 로그인 표시값을 세션에 저장
+			session.setAttribute("id", memberDTO.getMemberId());
+			// /board/main 주소변경하면서 이동
+			return "redirect:/main/main";
+		}else {
+			//아이디 비밀번호 틀림
+			//member/msg.jsp 주소변경없이 이동
+			return "/main/msg";
+		}
+		
+		
+	}	//loginPro();
+	
 	@GetMapping("/login1-1")
 	public String loginHome1() {
-		
+			
 		return "member/login1-1";
 	}
 	
