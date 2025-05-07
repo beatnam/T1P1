bae0b8a5512afe5f8f5cd484248b2eab98c9f2fd
gywjd <gywjd@LAPTOP-U7MK9NEQ>
2025-05-07 23:07:09 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'TeamTest' of https://github.com/beatnam/T1P1.git into
    Baekmi

--- Team1Project1/src/main/java/com/itwillbs/domain/CertificationDTO.java
+++ Team1Project1/src/main/java/com/itwillbs/domain/CertificationDTO.java
@@ -1,5 +1,7 @@
 package com.itwillbs.domain;
 
+import java.sql.Date;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,9 +12,13 @@ import lombok.ToString;
 public class CertificationDTO {
 
 	private int memberNum;
+	private int certificationId;
+	private int resumeID;;
 	private String certificationName;
 	private String certificationIssuer;
-	private String cermapAquiredDate;
+	private String certificationAcquiredDate;
+	private String certificationGrade;
+	private String certificationVerification;
 	
     
 }
